2b7414dd227ef42853b99ef84fbacd3ededde8e2
anxhukumar <contact.anshukumar@protonmail.com>
2025-05-01 17:48:32 +0530

    add text_node_to_html_node function in textnode.py and its test

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,8 +1,8 @@
 from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
-  def __init__(self, tag = None, value = None):
-    super().__init__(tag = tag, value = value)
+  def __init__(self, tag = None, value = None, props=None):
+    super().__init__(tag = tag, value = value, props=props)
   
   def to_html(self):
     if self.value:
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,6 +18,50 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.ITALIC)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold text")
+
+    def test_italic(self):
+        node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic text")
+
+    def test_code(self):
+        node = TextNode("print('Hello')", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "print('Hello')")
+
+    def test_link(self):
+        node = TextNode("OpenAI", TextType.LINK, "https://openai.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "OpenAI")
+        self.assertEqual(html_node.props, {"href": "https://openai.com"})
+
+    def test_image(self):
+        node = TextNode("Alt text", TextType.IMAGE, "https://example.com/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "https://example.com/image.png", "alt": "Alt text"})
+
+    def test_invalid_type(self):
+        class FakeType: pass
+        node = TextNode("Invalid type", FakeType())
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
 
 if __name__ == "__main__":
     unittest.main()
